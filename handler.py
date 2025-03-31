51830151f6c5f50176e9b5ab8f537de022863103
Joel Fong <joelfong@gmail.com>
2025-03-31 15:48:40 +0800

    Add serverless framework scaffold code

---
+++ handler.py
@@ -0,0 +1,28 @@
+import json
+
+
+def hello(event, context):
+    # uncomment
+    # print("✅ Lambda triggered by EventBridge schedule at UTC time:", context.aws_request_id)
+
+    
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
